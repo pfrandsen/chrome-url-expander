491ee9aac7b89b8f231935bfb7a0e394e19fa508
pfrandsen <pfrandsen@3cf63918-d5b2-bd4f-c4ab-62380e763bf0>
2011-01-09 16:27:51 +0000

    Branch for adding https matching

---
+++ https/expand.js
@@ -0,0 +1,50 @@
+/*
+ * Chrome URL Expander content script
+ *
+ * Copyright 2011, Peter Frandsen (pfrandsen@gmail.com)
+ * Licensed under the Apache 2.0 license (http://www.apache.org/licenses/LICENSE-2.0)
+ *
+*/
+var _expander = { "cur" : 0, "posX" : 0, "posY" : 0, "ref" : {} }
+
+document.addEventListener('mousemove', function (e) {
+  _expander.posX = e.clientX;
+  _expander.posY = e.clientY;
+}, false);
+
+document.addEventListener('keydown', function (e) {
+  console.log("keydown event 5");
+  if (e.keyCode == 17) {  // ctrl pressed
+    var target = document.elementFromPoint(_expander.posX, _expander.posY);
+    if (target) {
+      anchor = (function (el) {return el.href ? el : el.parentNode ? arguments.callee(el.parentNode) : null;})(target);
+      if (anchor) {
+        if (anchor.getAttribute("data-expanded")) {  // url already expanded - toggle href
+          if (anchor.getAttribute("data-expanded") == anchor.href) {
+            anchor.href = anchor.getAttribute("data-href");
+          } else {
+            anchor.href = anchor.getAttribute("data-expanded");
+          }
+        } else {  // expand the url if it matches url shortener pattern
+          urlRegEx = /(http:\/\/)(\w{1,8}\.\w{1,3})\/\w{1,10}$/i;
+          if (urlRegEx.test(anchor.href)) {
+            console.log("matched, expanding: " + anchor.href);
+            var key = _expander.cur++;
+            _expander.ref[key] = anchor;
+            chrome.extension.sendRequest({'url' : anchor.href, 'element' : key},
+              function (data) {
+                if (data.expanded) {
+                  var anchor = _expander.ref[data.element]
+                  // set custom attributes and change href to expanded url (will be shown in Status Bubble)
+                  anchor.setAttribute("data-href", anchor.href);
+                  anchor.href = data.expanded;
+                  anchor.setAttribute("data-expanded", anchor.href); // important to read href - a '/' may be added at end
+                }
+                delete _expander.ref[data.element];
+              });
+          }
+        }
+      }
+    }
+  }
+}, false);
